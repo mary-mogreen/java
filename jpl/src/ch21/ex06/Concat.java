e956ec68c0ac4f32a203efb622d9789f28f132c9
Katoh Yuuya <my_rn@me.com>
2016-11-06 12:38:28 +0900

    ファイルを一つずつ開くように修正 Enumrationの実装を追加

--- jpl/src/ch21/ex06/Concat.java
+++ jpl/src/ch21/ex06/Concat.java
@@ -5,6 +5,7 @@ package ch21.ex06;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
@@ -12,32 +13,76 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author mary-mogreen
  *
  */
 public class Concat {
-	public static void main(String[] args) throws IOException {
+	
+	@SuppressWarnings("resource")
+	public static void cat(String... args) throws IOException {
 		InputStream in;
-		if (args.length == 0) {
-			in = System.in;
-		} else {
-			InputStream fileIn, bufIn;
-			List<InputStream> inputs = new ArrayList<InputStream>(args.length);
-			for (String arg: args) {
-				fileIn = new FileInputStream(arg);
-				bufIn = new BufferedInputStream(fileIn);
-				inputs.add(bufIn);
-			}
-			Enumeration<InputStream> files = Collections.enumeration(inputs);
-			while (files.hasMoreElements()) {
-				in = files.nextElement();
-				int ch;
-				while ((ch = in.read()) != -1)
-					System.out.write(ch);
-			}
+		if (args.length == 0)
+			throw new IllegalArgumentException("1つ以上のファイルパス");
+		Enumeration<InputStream> files = new ConcatEnumration(args);
+		in = new SequenceInputStream(files);
+		int ch;
+		while ((ch = in.read()) != -1)
+			System.out.write(ch);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length > 0)
+			cat(args);
+		else {
+			String file1 = "/Users/katouyuuya/git/java_training2015/jpl/src/ch21/ex06/testfile.txt";
+			String file2 = "/Users/katouyuuya/git/java_training2015/jpl/src/ch21/ex06/testfile2.txt";
+			String[] files = {file1, file2};
+			cat(files);
+		}
+	}
+	
+	static class ConcatEnumration implements Enumeration<InputStream> {
+
+		private String[] files;
+		private int index;
+		private InputStream in;
+		
+		ConcatEnumration(String[] files) {
+			this.files = files.clone();
 		}
 		
-	}
+		@Override
+		public boolean hasMoreElements() {
+			return index < files.length;
+		}
+
+		@Override
+		public InputStream nextElement() {
+			if (!hasMoreElements())
+				throw new NoSuchElementException("nothing");
+			if (in != null) {
+				try {
+					in.close();
+					System.out.printf("%n[%s] close.%n ", files[index]);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			InputStream fileIn;
+			try {
+				System.out.printf("%n[%s] open.%n ", files[index]);
+				fileIn = new FileInputStream(files[index++]);
+				in = new BufferedInputStream(fileIn);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			return in;
+		}
+		
+	} 
 }
