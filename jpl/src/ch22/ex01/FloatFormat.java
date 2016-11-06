bdcdcab38e28c01f9ccd8e4467ad19042cac8cf9
Katoh Yuuya <my_rn@me.com>
2016-11-06 13:19:49 +0900

    コマンドライン引数を受け付けるよう修正

--- jpl/src/ch22/ex01/FloatFormat.java
+++ jpl/src/ch22/ex01/FloatFormat.java
@@ -23,6 +23,20 @@ public class FloatFormat {
 	}
 	
 	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("testを実行");
+			test();
+		} else {
+			float[] floats = new float[args.length - 1];
+			for (int i = 0; i < args.length - 1; i++) {
+				floats[i] = Float.parseFloat(args[i]);
+			}
+			int cols = Integer.parseInt(args[args.length - 1]);
+			FloatFormat.printFloats(floats, cols);
+		}
+	}
+	
+	public static void test() {
 		FloatFormat.printFloats(new float[] {
 				1.234f, 34.55555f, 543f, -87654.453f, 0.0000432f, 543.2507f, -22.222222222455f, 8.24252343634f, -423654848.54f 
 		}, 2);
